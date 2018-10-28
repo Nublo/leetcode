a26e55079b15ab0da6379d4132d792147f96019a
Nublo <varyvonchyk.anatol@gmail.com>
2018-10-28 17:55:16 +0000

    4th problem solved.

---
+++ src/main/kotlin/4_longest_substring_without_repeat.kt
@@ -0,0 +1,31 @@
+import java.util.HashSet
+
+/**
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Add char to tempAnswer if it is not in set, otherwise, start cutting first symbols from tempAnswer
+ *
+ * O(n)
+ */
+fun lengthOfLongestSubstring(s: String): Int {
+    val set = HashSet<Char>()
+    var ans = StringBuilder()
+    val cur = StringBuilder()
+    for (i in 0 until s.length) {
+        if (set.contains(s[i])) {
+            if (cur.length > ans.length) {
+                ans = StringBuilder(cur)
+            }
+            while (set.contains(s[i])) {
+                set.remove(cur.first())
+                cur.deleteCharAt(0)
+            }
+        }
+        set.add(s[i])
+        cur.append(s[i])
+    }
+    if (cur.length > ans.length) {
+        ans = StringBuilder(cur)
+    }
+    return ans.length
+}
