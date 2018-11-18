6cbe12d5d65460805595b8fafb82a099e0658095
Nublo <varyvonchyk.anatol@gmail.com>
2018-11-18 12:56:40 +0000

    Solved 23 merge K sorted lists.

---
+++ src/main/kotlin/23_merge_k_sorted_lists.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+/**
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ *
+ * Solution:
+ * Just put heads of lists in priorityQueue and iterate while queue is not empty
+ *
+ * O(N*logK)
+ * N - length of lists
+ * K - amount of lists
+ */
+
+fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+    val dumbHead = ListNode(0)
+    var current: ListNode? = dumbHead
+    val priorityQueue: PriorityQueue<ListNode> = PriorityQueue { a, b -> a.`val` - b.`val` }
+    priorityQueue.addAll(lists.filter { it != null }.map { it as ListNode })
+    while (priorityQueue.isNotEmpty()) {
+        val queueHead: ListNode = priorityQueue.poll()
+        current?.next = ListNode(queueHead.`val`)
+        current = current?.next
+        queueHead.next?.let { priorityQueue.add(it) }
+    }
+    return dumbHead.next
+}
